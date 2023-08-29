fc4366187eb4cc765d4954dc71df49d2db5d63f2
cash <cash@cashwu.com>
2023-08-29 14:15:50 +0800

    update auth api

--- NetCore8AuthApi/Program.cs
+++ NetCore8AuthApi/Program.cs
@@ -1,7 +1,48 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer",
+                                  new OpenApiSecurityScheme
+                                  {
+                                      Name = "Authorization",
+                                      In = ParameterLocation.Header,
+                                      Type = SecuritySchemeType.Http,
+                                      Scheme = "Bearer"
+                                  });
+    
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorizationBuilder();
+builder.Services.AddDbContext<AppDbContext>(a => a.UseSqlite("DataSource=app.db"));
+
+builder.Services.AddIdentityCore<MyUser>()
+       .AddEntityFrameworkStores<AppDbContext>()
+       .AddApiEndpoints();
 
 var app = builder.Build();
 
@@ -11,6 +52,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapIdentityApi<MyUser>();
 app.MapGet("/", () => "OK");
 
 app.Run();
+
+class MyUser : IdentityUser
+{
+}
+
+class AppDbContext : IdentityDbContext<MyUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+}
